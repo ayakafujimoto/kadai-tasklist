7962c75bb123e46a3614802ef94b32fd60116d75
ayaka.fujimoto <fujimoto@coresite.ne.jp>
2021-04-12 08:23:38 +0000

    kadai-tasklist

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -134,3 +134,4 @@ class TasksController extends Controller
         return redirect('/');
     }
 }
+
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,3 +26,4 @@ class AppServiceProvider extends ServiceProvider
         \URL::forceScheme('https');
     }
 }
+
